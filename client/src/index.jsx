3c0a7b1e1823366a0734173dc61dcfbb1e5deafe
Kelly Qu <116982102+kellyquu@users.noreply.github.com>
2024-01-13 14:42:37 -0500

    implemented radarchart

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'chart.js/auto';
+import './App.css'; // Path to your main CSS file
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root')
