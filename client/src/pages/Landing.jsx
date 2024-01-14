2a9a3e12ae47ebdf30f59afeffe3d0ebb7cb2794
roskzhu <roskenzhu@gmail.com>
2024-01-13 23:03:20 -0500

    styling fonts

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -6,23 +6,26 @@ const Landing = () => {
   };
 
   return (
-    <div className="content">
-    <div className='gradient-container'>
-        <h1 className='gradient-text'>empa</h1>
-    </div>
-    <h2 className='subtitle'>your emotional intelligence partner</h2>
-    <div className='blurb'>
-        <p> Unlock the power of non-verbal communication with Empa – the cutting-edge tool designed to decode emotions in audio and video expressions. 
-            Gain real-time insights into the unspoken language, receiving personalized feedback to enhance your communication skills in virtual meetings, presentations, and interviews. 
-            Empa not only deciphers emotions but provides actionable insights on addressing specific feelings effectively, empowering you to build stronger connections. 
-        </p>
-    </div>
-        <button onClick={redirectToMain}>
-            click (MAYA FIX BUTTON)
-        </button>
-      <div className='blob-container'>
-        <img className='blob' src='/blob1.png' alt='Image Description'/>
+    <div className="bg-gradient-to-r from-white via-white to-blue-100 m-0 p-20 v">
+      <h2 className='subtitle'>
+        Your Emotional Intelligence partner
+      </h2>
+      <div className='gradient-container my-70'>
+        Meet
+          <h1 className='gradient-text text-9xl my-8'>empa</h1>
       </div>
+      <div className='blurb'>
+          {/* <p> Unlock the power of non-verbal communication with Empa – the cutting-edge tool designed to decode emotions in audio and video expressions. 
+              Gain real-time insights into the unspoken language, receiving personalized feedback to enhance your communication skills in virtual meetings, presentations, and  Chillaxviews. 
+              Empa not only deciphers emotions but provides actionable insights on addressing specific feelings effectively, empowering you to build stronger connections. 
+          </p> */}
+      </div>
+          <button onClick={redirectToMain}>
+              click (MAYA FIX BUTTON)
+          </button>
+        <div className='blob-container'>
+          <img className='blob' src='/blob1.png' alt='Image Description'/>
+        </div>
     </div>
   );
 };
