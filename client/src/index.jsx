54951ea48500833e3284ec7984f2ccd5c358fd2c
roskzhu <roskenzhu@gmail.com>
2024-01-13 20:18:21 -0500

    merge conflict res

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'chart.js/auto';
+// import 'chart.js/auto';
 import './App.css'; // Path to your main CSS file
 
 const root = ReactDOM.createRoot(
