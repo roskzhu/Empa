b148a77b6b83780f601593d573a90cb58fdc1b43
roskzhu <roskenzhu@gmail.com>
2024-01-14 00:42:01 -0500

    font cleaned and spacing fix on landing

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Carousel from '../components/carousel/CarouselWrapper';
 
 const Landing = () => {
   const redirectToMain = () => {
@@ -6,29 +7,35 @@ const Landing = () => {
   };
 
   return (
-    <div className="background-gradient m-0 p-20 v">
-      <div className='text-3xl mt-40 font-semibold border-black/80 border-2 rounded-full px-6 py-2'>
-        Meet <span className="gradient-text my-8">empa</span>
+    <div className="background-gradient m-0 p-20 h-screen flex">
+      <div>
+      <div className='text-2xl mt-[240px] font-extrabold border-black/80 border-[3px] 
+                      rounded-full px-4 py-1 text-black/80 w-[200px]'>
+        Meet <span className="gradient-text my-8 pr-2">empa</span> {'ᐳ'}
       </div>
       
-      <h2 className='text-black/85 font-semibold text-8xl w-[60%] py-10 leading-snug'>
-        Your Emotional Intelligence Partner
+      <h2 className='text-black/85 font-extrabold text-7xl w-[50%] py-6 leading-snug'>
+        Your Emotional Intelligence Partner.
       </h2>
 
       <div className='blurb'>
-          <p className="text-gray text-2xl"> 
+          <p className="text-black/60 text-3xl leading-relaxed font-medium pb-10 w-1/2"> 
             {/* Unlock the power of non-verbal communication with Empa – the cutting-edge tool designed to decode emotions in audio and video expressions.  */}
-            Gain real-time insights into the unspoken language, receiving personalized feedback to enhance your communication skills in virtual meetings, presentations, and  Satoshiviews. 
+            Gain real-time insights into the unspoken language, receiving personalized feedback to
+            enhance your communication skills in virtual meetings, presentations, and interviews. 
             {/* Empa not only deciphers emotions but provides actionable insights on addressing specific feelings effectively, empowering you to build stronger connections.  */}
           </p>
       </div>
-      <button className='explore-button font-medium rounded-full text-3xl p-4 px-8' 
+      <button className='explore-button font-semibold rounded-full text-3xl p-6 px-9' 
               onClick={redirectToMain}>
-            explore
+            Explore now
         </button>
-        <div className='blob-container absolute'>
-          <img className='blob' src='/blob1.png' alt='Image Description'/>
         </div>
+        <div className='absolute justify-end w-full flex align-center pt-60 pr-60'>
+          <img className='w-1/3' src='/blob1.png' alt='Image Description'/>
+        </div>
+
+        <Carousel/>
     </div>
   );
 };
