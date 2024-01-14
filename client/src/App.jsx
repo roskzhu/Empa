d6331b529e8056c660113c64089ae6671ba2e412
roskzhu <roskenzhu@gmail.com>
2024-01-13 20:18:21 -0500

    merge conflict res

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import {  BrowserRouter,  Route,  Routes} from "react-router-dom";
 import Main from "./pages/Main";
+import Landing from "./pages/Landing";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/main" element={<Main />} />
       </Routes>
     </BrowserRouter>
   );
--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -241,59 +241,5 @@ function Main() {
     </center>
   );
 }
-=======
-import React, { useEffect, useRef } from 'react';
-import createEmotionRadarChart from '../components/emotionRadar.js';
-import '../components/emotionRadar.css';
-import CameraFeed from '../components/CameraFeed.js';
-import '../components/CameraFeed.css';
-
-const Main = () => {
-  const canvasRef = useRef(null);
-  const chartRef = useRef(null);
-
-  useEffect(() => {
-    // Dummy emotion data
-    const emotionData = [60, 30, 10, 7, 3, 0, 0];
-
-    if (canvasRef.current) {
-      // Destroy the previous chart
-      if (chartRef.current) {
-        chartRef.current.destroy();
-      }
-
-      // Create a new chart
-      chartRef.current = createEmotionRadarChart(canvasRef.current, emotionData);
-    }
-
-    // Cleanup function to destroy chart when component unmounts
-    return () => {
-      if (chartRef.current) {
-        chartRef.current.destroy();
-      }
-    };
-  }, []); // Add dependencies if needed
-
-  return (
-    <div className="main-container">
-      <div className="camera-container">
-        <CameraFeed />
-      </div>
-      <div className="sidebar">
-        <div className='logo'>
-          <h1>empa</h1>
-        </div>
-        <canvas ref={canvasRef} className="emotion-radar-canvas"></canvas>
-        <div>
-          <p>audio transcription to go here</p>
-        </div>
-        <div>
-          <p>OpenAI info to go here</p>
-        </div>
-      </div>
-    </div>
-  );
-  
-};
 
 export default Main;
