8410bb1f8f02dbf8c04c767b5e1166b541751ed0
Kelly Qu <116982102+kellyquu@users.noreply.github.com>
2024-01-14 08:21:41 -0500

    added icons to carousel

--- client/src/components/carousel/CarouselCard.jsx
+++ client/src/components/carousel/CarouselCard.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import styled from '@emotion/styled'
 import './carousel.css'; 
 
-const CarouselCard = ({card}) => {
+const CarouselCard = ({ card }) => {
   return (
     <div className="m-1 rounded-xl hover:bg-black/10 p-5 min-h-[440px]">
       <div className="reverse-gradient h-[170px]">
-        img here
+        <img src={card.image} alt={card.content} />
       </div>
       <p className='text-3xl font-extrabold my-10 text-black/80'>{card.content}</p>
       <p className='answer'>{card.answer}</p>
     </div>
   )
 }
+
 export default CarouselCard
