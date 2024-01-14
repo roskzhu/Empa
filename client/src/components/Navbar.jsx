c164af648281ee14a62f880941d452511a2df15e
roskzhu <roskenzhu@gmail.com>
2024-01-14 00:42:01 -0500

    font cleaned and spacing fix on landing

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="flex w-full items-center justify-between px-12 py-4 text-2xl bg-transparent fixed top-0 bg-white">
+    <nav className="flex w-full items-center justify-between px-12 py-6 text-2xl bg-transparent fixed top-0 bg-white">
       <Link to="/">
-        <h1 className="text-4xl font-semibold gradient-text">empa</h1>
+        <h1 className="text-4xl gradient-text font-extrabold ">empa</h1>
       </Link>
-      <ul className="flex p-3 px-8 rounded-full text-black font-regular">
+      <ul className="flex p-4 px-8 rounded-full text-white bg-blue-700 font-semibold">
         <Link to="/main">
           <li className="">Try Now</li>
         </Link>
