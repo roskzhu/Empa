a3ae1e3ae6e8f420fea7a9b071f77bc05385f4e9
Kelly Qu <116982102+kellyquu@users.noreply.github.com>
2024-01-14 08:43:40 -0500

    cented imgs

--- client/src/components/carousel/CarouselCard.jsx
+++ client/src/components/carousel/CarouselCard.jsx
@@ -4,9 +4,9 @@ import './carousel.css';
 
 const CarouselCard = ({ card }) => {
   return (
-    <div className="m-1 rounded-xl hover:bg-black/10 p-5 min-h-[440px]">
+    <div className="m-1 rounded-xl hover:bg-black/10 p-5 min-h-[440px] justify-content align-center items-center">
       <div className="reverse-gradient h-[170px]">
-        <img src={card.image} alt={card.content} />
+        <img className="w-1/4 justify-center ml-[160px] pt-[30px]" src={card.image} alt={card.content} />
       </div>
       <p className='text-3xl font-extrabold my-10 text-black/80'>{card.content}</p>
       <p className='answer'>{card.answer}</p>
