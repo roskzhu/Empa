8b5c0191f2edc23b27b5ccf0ea03a475928b5266
roskzhu <roskenzhu@gmail.com>
2024-01-13 23:03:20 -0500

    styling fonts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import {  BrowserRouter,  Route,  Routes} from "react-router-dom";
 import Main from "./pages/Main";
 import Landing from "./pages/Landing";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/main" element={<Main />} />
